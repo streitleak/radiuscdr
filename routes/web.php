d860aac145c2182d607d9a150d773c31122e538f
StreitLeak <streitleak@gmail.com>
2021-04-06 00:36:16 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/index');
+#Route::get('/', function () {
+#    return view('welcome');
+#});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
